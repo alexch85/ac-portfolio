675af40d06a174408de37bdeef4dfe0c3aaee9ca
Alex <alexch85@windowslive.com>
2020-09-19 13:46:21 +0200

    medias for tablets added

--- src/features/pages/aboutPage/AboutPage.jsx
+++ src/features/pages/aboutPage/AboutPage.jsx
@@ -6,21 +6,23 @@ import { FaFileDownload } from 'react-icons/fa';
 export default function AboutPage() {
 	return (
 		<div className={styles.AboutPage} id='about'>
-			<div className={styles.PhotoGradient}></div>
-			<img className={styles.AcPhoto} alt='Alex' src='/pictures/ac-photo.png' />
-			<div className={styles.AboutDescription}>
-				<h1>About me</h1>
-				<p>
-					Eu in est eiusmod nisi officia reprehenderit est sint occaecat tempor est elit irure excepteur.
-					Culpa commodo dolore labore esse eu ea consectetur ut voluptate do. Proident in sunt qui ipsum
-					proident culpa in excepteur fugiat qui sit nostrud.
-				</p>
-				<Button costumColor='white'>
-					<FaFileDownload style={{ marginRight: '3px' }} /> download resume
-				</Button>
-			</div>
-			<div className={styles.AboutPageGraphics}>
-				<img alt='about graphics' src='/svg-files/about-graphics.svg' />
+			<div className={styles.AboutPageContent}>
+				{/* <div className={styles.PhotoGradient}></div> */}
+				<img className={styles.AcPhoto} alt='Alex' src='/pictures/ac-photo.png' />
+				<div className={styles.AboutDescription}>
+					<h1>About me</h1>
+					<p>
+						Eu in est eiusmod nisi officia reprehenderit est sint occaecat tempor est elit irure excepteur.
+						Culpa commodo dolore labore esse eu ea consectetur ut voluptate do. Proident in sunt qui ipsum
+						proident culpa in excepteur fugiat qui sit nostrud.
+					</p>
+					<Button costumColor='white'>
+						<FaFileDownload style={{ marginRight: '3px' }} /> download resume
+					</Button>
+				</div>
+				<div className={styles.AboutPageGraphics}>
+					<img alt='about graphics' src='/svg-files/about-graphics.svg' />
+				</div>
 			</div>
 		</div>
 	);
